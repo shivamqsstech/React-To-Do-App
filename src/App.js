c6c99df9a87e208fbd46c63dacc8a7771a5cf0ff
Shivam Singh <74213016+shivamqsstech@users.noreply.github.com>
2024-12-24 18:25:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ToDo from './Components/to-do';
+import TodoItems from './Components/TodoItems';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <ToDo></ToDo>
+      <TodoItems/>
+
+    </div>
+  );
+}
+
+export default App;
